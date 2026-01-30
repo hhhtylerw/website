39db27c37c9534968e8959f31dd9d65e213772f4
hhhtylerw <hhhtylerw@protonmail.com>
2026-01-30 09:12:55 -0800

    Revert "Fix prerender"
    This reverts commit d9afa40b6e0566f36ddae1f8fc7f70070cfc9d78.

--- svelte.config.js
+++ svelte.config.js
@@ -13,19 +13,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '200.html'
-		}),
-		prerender: {
-			handleUnseenRoutes: 'ignore'
-		}
+		adapter: adapter()
 	},
 	compilerOptions: {
 		runes: true,
 		warningFilter: (warning) => warning.code !== 'script_context_deprecated'
 	},
 	extensions: ['.svelte', '.md'],
-	preprocess: [mdsvex(mdsvexOptions)]
+	preprocess: [mdsvex(mdsvexOptions)],
+	kit: {
+		prerender: {
+			handleUnseenRoutes: 'ignore'
+		}
+	}
 };
 
 export default config;
